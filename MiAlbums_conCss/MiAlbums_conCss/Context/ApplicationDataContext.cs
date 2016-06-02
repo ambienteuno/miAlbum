6ea1ca909247a484d1892bd6e04998d0a8eac2d6
Matias Carrion <Matias12_12@hotmail.com>
2016-06-02 17:34:46 -0300

    Agrego nuevo proyecto MVC con CSS

---
+++ MiAlbums_conCss/MiAlbums_conCss/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MiAlbums_conCss.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MiAlbums_conCss.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ MiAlbums_conCss/MiAlbums_conCss/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiAlbums_conCss.MvcApplication" Language="C#" %>
